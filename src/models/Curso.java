9af4a5602b8fe015bd21b15ce33b424dc7a7804f
Ailan Jimenez <ailan.jimenez.valverde@est.una.ac.cr>
2025-04-17 00:31:05 -0600

    Agregacion de el CRUD de cursos

--- src/models/Curso.java
+++ src/models/Curso.java
@@ -1,5 +1,27 @@
 package models;
 
 public class Curso {
+	private char sigla;
+	private String nombreCurso;
+	
+	public Curso(char pSigla,String pNombreCurso) {
+		sigla=pSigla;
+		nombreCurso=pNombreCurso;
+	}
 
+	public char getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(char pSigla) {
+		this.sigla = pSigla;
+	}
+
+	public String getNombreCurso() {
+		return nombreCurso;
+	}
+
+	public void setNombreCurso(String pNombreCurso) {
+		this.nombreCurso = pNombreCurso;
+	}
 }
--- src/models/Logic.java
+++ src/models/Logic.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Logic {
     private Universidad u;
     
@@ -31,5 +33,110 @@ public class Logic {
     public void actualizarTelefono(String pTelefono) {
         u.setTelefonoU(pTelefono);
     }
-    //Aca abajo iran los metodos de los cursos
+    
+    // Metodos CRUD para cursos de escuelas
+    
+    public void agregarCurso(Curso pCurso, Escuela pEscuela) {
+    	pEscuela.getVector().add(pCurso);
+    }
+    
+    public boolean eliminarCurso(String nombreCurso,Escuela pEscuela) {
+    	ArrayList <Curso> cursos =pEscuela.getVector();
+    	for(int i=0; i< cursos.size(); i++) {
+    		if(cursos.get(i).getNombreCurso().equalsIgnoreCase(nombreCurso)) {
+    			cursos.remove(i);
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public boolean modificarCurso(String nombreCurso,Escuela pEscuela, String nuevoNombreCurso) {
+    	ArrayList <Curso> cursos =pEscuela.getVector();
+    	for (Curso curso : cursos) {
+            if (curso.getNombreCurso().equalsIgnoreCase(nombreCurso)) {
+                curso.setNombreCurso(nuevoNombreCurso);
+                return true;
+            }
+    	}
+    	return false;
+    }
+    
+    public String consultarCurso(Escuela pEscuela, String nombreCurso) {
+        ArrayList<Curso> cursos = pEscuela.getVector(); // Obtienes el vector de cursos de la escuela
+        
+        for (Curso curso : cursos) {
+            if (curso.getNombreCurso().equalsIgnoreCase(nombreCurso)) {
+                return "Nombre: " + curso.getNombreCurso() + ", Sigla: " + curso.getSigla();
+            }
+        }
+        
+        return "Curso no encontrado.";
+    }
+    
+    public String consultarCursosDeEscuela(Escuela pEscuela) {
+        StringBuilder resultado = new StringBuilder();
+        
+        ArrayList<Curso> cursos = pEscuela.getVector(); // Obtienes el vector de cursos de la escuela
+        if (cursos.isEmpty()) {
+            return "La escuela " + pEscuela.getNombre() + " no tiene cursos registrados.";
+        }
+
+        resultado.append("Escuela: ").append(pEscuela.getNombre()).append("\n");
+        for (Curso curso : cursos) {
+            resultado.append(" - Nombre: ").append(curso.getNombreCurso())
+                     .append(", Sigla: ").append(curso.getSigla()).append("\n");
+        }
+
+        return resultado.toString();
+    }
+    
+    
+    public String consultarCursosUniversidad(Universidad pUniversidad) {
+    	StringBuilder resultado = new StringBuilder();
+    	
+    	ArrayList <Escuela> escuelas =pUniversidad.getVector();
+    	 if (escuelas.isEmpty()) {
+    	        return "No hay escuelas registradas.";
+    	    }
+    	 
+    	 for (Escuela escuela : escuelas) {
+    	        ArrayList<Curso> cursos = escuela.getVector(); // Vector de cursos de la escuela actual
+    	        if (cursos.isEmpty()) {
+    	            resultado.append("Escuela: ").append(escuela.getNombre()).append(" no tiene cursos registrados.\n");
+    	        } else {
+    	            resultado.append("Escuela: ").append(escuela.getNombre()).append("\n");
+    	            for (Curso curso : cursos) {
+    	                resultado.append(" - Nombre: ").append(curso.getNombreCurso())
+    	                         .append(", Sigla: ").append(curso.getSigla()).append("\n");
+    	            }
+    	        }
+    	        resultado.append("\n"); // Salto de línea para separar las escuelas
+    	    }
+
+    	    return resultado.toString();
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
